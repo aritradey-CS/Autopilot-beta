9567958ec486fa7fb311a8f311b4a44720ffc077
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-01 23:18:21 +0530

    car card added No.01

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,13 +1,44 @@
-// src/components/CarList.js
 import React from "react";
-import carsData from "../carsData";
+import "./CarList.css"; // Import your CSS file for styling
 
 function CarList() {
-  // Your code for rendering the list of car cards goes here
+  // Sample car data (you can replace this with your actual data)
+  const cars = [
+    {
+      id: 1,
+      name: "Car 1",
+      image: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80", // Replace with your image URL
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 2,
+      name: "Car 2",
+      image: "https://images.unsplash.com/photo-1549927681-0b673b8243ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80", // Replace with your image URL
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 3,
+      name: "Car 3",
+      image: "https://images.unsplash.com/photo-1542282088-72c9c27ed0cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80", // Replace with your image URL
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    // Add more car data as needed
+  ];
 
   return (
     <div className="car-list">
-      {/* Render car cards here */}
+      {cars.map((car) => (
+        <div className="car-card" key={car.id}>
+          <div
+            className="car-image"
+            style={{ backgroundImage: `url(${car.image})` }}
+          ></div>
+          <div className="car-details">
+            <h4>{car.name}</h4>
+            <p>{car.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
