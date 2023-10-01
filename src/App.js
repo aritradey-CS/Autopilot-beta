8aba43c606d24402ad0c7a7686e363a9eee7db70
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-01 11:53:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/components/App.js
+import React from "react";
+import carsData from "./components/carsData"; // Import your mock data here
+
+function App() {
+  // Your code for managing state, pagination, and search will go here
+
+  return (
+    <div className="App">
+      {/* Your components go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/carsData.jsx
@@ -0,0 +1,9 @@
+// src/carsData.js
+const carsData = [
+    { id: 1, name: "Car 1", image: "car1.jpg" },
+    { id: 2, name: "Car 2", image: "car2.jpg" },
+    // Add more car data here
+  ];
+  
+  export default carsData;
+  
