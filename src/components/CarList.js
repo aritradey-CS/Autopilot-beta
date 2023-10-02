d530f228e9b33b60e20d89df97cf9457dbac9cc5
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-02 17:17:14 +0530

    pagination added only some bugs are their

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CarList.css"; // Import your CSS file for styling
 import carsData from "./carsData.json";
 
 function CarList() {
+  // Define the number of cars to display per page
+  const carsPerPage = 6;
+
+  // Initialize the current page state
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calculate the index of the first and last car for the current page
+  const indexOfLastCar = currentPage * carsPerPage;
+  const indexOfFirstCar = indexOfLastCar - carsPerPage;
+
+  // Slice the carsData array to get cars for the current page
+  const currentCars = carsData.slice(indexOfFirstCar, indexOfLastCar);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(carsData.length / carsPerPage);
+
+  // Function to handle page change when clicking page numbers
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  // Function to handle next page click
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Function to handle previous page click
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div className="car-list">
-      {carsData.map((car) => (
+      {currentCars.map((car) => (
         <div className="car-card" key={car.id}>
           <div
             className="car-image"
@@ -16,30 +51,56 @@ function CarList() {
               <h2>{car.name}</h2>
               <h3>{car.year}</h3>
             </div>
-            
-          <div className="car-config">
-            <div className="config"><i class="ri-group-fill"></i> People {car.passengerCapacity}</div>
-            <div className="config"><i class="ri-dashboard-3-fill"></i>  {car.mileage}</div>
-            <div className="config"><i class="ri-steering-2-fill"></i>  {car.transmission}</div>
-            <div className="config"><i class="ri-dashboard-2-fill"></i>  {car.fuelType}</div>
-
-          </div>
-
-                    
-            <div className="car-footer">
-              <div className="price">
-                {car.pricePerMonth} / month
+            <div className="car-config">
+              <div className="config">
+                <i class="ri-group-fill"></i> People {car.passengerCapacity}
               </div>
-              <div className="buttons">
-                <button className="love-button"> 
-               <button className="rent-button">Rent Now</button>
-                </button>
+              <div className="config">
+                <i class="ri-dashboard-3-fill"></i> {car.mileage}
+              </div>
+              <div className="config">
+                <i class="ri-steering-2-fill"></i> {car.transmission}
+              </div>
+              <div className="config">
+                <i class="ri-dashboard-2-fill"></i> {car.fuelType}
               </div>
             </div>
-            <i class="ri-heart-3-fill"></i> ({car.loveCount})
+            <div className="car-footer">
+              <div className="price">{car.pricePerMonth} / month</div>
+              <div className="buttons">
+                <button className="love-button">
+                  <i class="ri-heart-3-fill"></i> ({car.loveCount})
+                </button>
+                <button className="rent-button">Rent Now</button>
+              </div>
+            </div>
           </div>
         </div>
       ))}
+
+      <div className="pagination">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1} // Disable the previous button on the first page
+        >
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => handlePageChange(i + 1)}
+            className={currentPage === i + 1 ? "active" : ""}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages} // Disable the next button on the last page
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,53 @@
-// src/components/Pagination.js
-import React from "react";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function Pagination({ currentPage, totalPages, onPageChange }) {
-  // Your code for pagination controls goes here
+function Pagination() {
+  const history = useHistory();
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10; // Hardcoded total number of pages
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    history.push(`/page/${page}`);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      handlePageChange(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      handlePageChange(currentPage - 1);
+    }
+  };
+
+  const renderPaginationNumbers = () => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(
+        <span
+          key={i}
+          className={`page-number ${i === currentPage ? 'active' : ''}`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </span>
+      );
+    }
+    return pages;
+  };
 
   return (
     <div className="pagination">
-      {/* Render pagination controls here */}
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        Previous
+      </button>
+      {renderPaginationNumbers()}
+      <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+        Next
+      </button>
     </div>
   );
 }
