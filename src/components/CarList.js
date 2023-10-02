b994b32165f8661e5c608cbf336bec50da4f871d
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-02 11:17:28 +0530

    added entries to carsData.json

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,39 +1,11 @@
 import React from "react";
 import "./CarList.css"; // Import your CSS file for styling
+import carsData from "./carsData.json"; // Correct the import path
 
 function CarList() {
-  // Sample car data (you can replace this with your actual data)
-  const cars = [
-    {
-      id: 1,
-      name: "Car 1",
-      image: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80", // Replace with your image URL
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      id: 2,
-      name: "Car 2",
-      image: "https://images.unsplash.com/photo-1549927681-0b673b8243ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80", // Replace with your image URL
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      id: 3,
-      name: "Car 3",
-      image: "https://images.unsplash.com/photo-1542282088-72c9c27ed0cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80", // Replace with your image URL
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      id: 3,
-      name: "Car 4",
-      image: "https://images.unsplash.com/photo-1602034032025-47f18f22244f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80", // Replace with your image URL
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    // Add more car data as needed
-  ];
-
   return (
     <div className="car-list">
-      {cars.map((car) => (
+      {carsData.map((car) => (
         <div className="car-card" key={car.id}>
           <div
             className="car-image"
