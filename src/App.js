8bfe393e6858d60e439ace929575b1a36a059b1a
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-02 09:10:18 +0530

    smooth scroll added locomotive+gsap using codepen

--- src/App.js
+++ src/App.js
@@ -1,63 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CarList from "./components/CarList";
 import CarDetails from "./components/CarDetails";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger"; // Import ScrollTrigger
+import LocomotiveScroll from "locomotive-scroll"; // Import Locomotive Scroll
 import "./App.css";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState(""); // State to store the search term
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Initialize Locomotive Scroll once the component is mounted
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+
+    // Register ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Enable smooth scrolling and proxy methods
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+
+    // Update Locomotive Scroll when ScrollTrigger refreshes
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // Refresh ScrollTrigger and Locomotive Scroll
+    ScrollTrigger.refresh();
+
+    // Clean up Locomotive Scroll when the component unmounts
+    return () => {
+      locoScroll.destroy();
+    };
+  }, []);
 
   const handleSearchClick = () => {
-    // Perform the search action here using the searchTerm state
-    // For example, you can console.log the searchTerm for now
     console.log("Search term:", searchTerm);
   };
 
   return (
     <div className="App">
-      <header className="navbar">
-        <div className="search-section">
-          <div className="search-input-container">
-            <input
-              type="text"
-              placeholder="Search cars by name"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button className="search-button" onClick={handleSearchClick}>
-              <i className="ri-search-line"></i>
-            </button>
-          </div>
-        </div>
+      <div className="main" id="main">
+        <header className="navbar">
+          
+            <div className="search-section">
+              <div className="search-input-container">
+                <input
+                  type="text"
+                  placeholder="Search cars by name"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button className="search-button" onClick={handleSearchClick}>
+                  <i className="ri-search-line"></i>
+                </button>
+              </div>
+            </div>
 
-        <div className="dropdowns">
-          <div className="dropdown1">
-            <select id="category">
-              <option value="relevance">Relevance</option>
-              <option value="priceLowToHigh">Price Low to High</option>
-              <option value="priceHighToLow">Price High to Low</option>
-            </select>
-            <i className="ri-arrow-drop-down-line"></i>
+            <div className="dropdowns">
+              <div className="dropdown1">
+                <select id="category">
+                  <option value="relevance">Relevance</option>
+                  <option value="priceLowToHigh">Price Low to High</option>
+                  <option value="priceHighToLow">Price High to Low</option>
+                </select>
+                <i className="ri-arrow-drop-down-line"></i>
+              </div>
+              <div className="dropdown2">
+                <select id="brand">
+                  <option value="allBrands">All Brands</option>
+                  <option value="audi">Audi</option>
+                  <option value="mercedes">Mercedes</option>
+                  <option value="bmw">BMW</option>
+                </select>
+                <i className="ri-arrow-drop-down-line"></i>
+              </div>
+            </div>
+          </header>
+       
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route exact path="/" element={<CarList />} />
+              <Route path="/car/:id" element={<CarDetails />} />
+            </Routes>
           </div>
-          <div className="dropdown2">
-            <select id="brand">
-              <option value="allBrands">All Brands</option>
-              <option value="audi">Audi</option>
-              <option value="mercedes">Mercedes</option>
-              <option value="bmw">BMW</option>
-            </select>
-            <i className="ri-arrow-drop-down-line"></i>
-          </div>
-        </div>
-      </header>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route exact path="/" element={<CarList />} />
-            <Route path="/car/:id" element={<CarDetails />} />
-          </Routes>
-        </div>
-      </Router>
+        </Router>
+      </div>
     </div>
   );
 }
